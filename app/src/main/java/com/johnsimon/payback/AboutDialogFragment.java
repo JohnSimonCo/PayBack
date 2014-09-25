3cf2285fba8676a4a0a4d315f991cb94f041a140
Simon <swesnowme@gmail.com>
2014-09-25 21:34:47 +0200

    Work

--- app/src/main/java/com/johnsimon/payback/AboutDialogFragment.java
+++ app/src/main/java/com/johnsimon/payback/AboutDialogFragment.java
@@ -3,9 +3,11 @@ package com.johnsimon.payback;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.TextView;
 
 public class AboutDialogFragment extends DialogFragment {
 
@@ -16,6 +18,17 @@ public class AboutDialogFragment extends DialogFragment {
         LayoutInflater inflater = getActivity().getLayoutInflater();
 
         View rootView = inflater.inflate(R.layout.about_dialog_fragment, null);
+
+		TextView versionText = (TextView) rootView.findViewById(R.id.about_dialog_version);
+		try {
+			String version = getActivity().getPackageManager()
+					.getPackageInfo(getActivity().getPackageName(), 0).versionName;
+			versionText.setText("v" + version);
+		} catch (PackageManager.NameNotFoundException e) {
+			e.printStackTrace();
+			versionText.setText("");
+		};
+
 
         builder.setView(rootView);
 
