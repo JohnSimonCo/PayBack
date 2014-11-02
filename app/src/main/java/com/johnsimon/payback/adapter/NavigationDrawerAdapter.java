2adf78204e045b232327e45e8d023c2b6c42ace8
Simon <swesnowme@gmail.com>
2014-11-02 17:56:45 +0100

    Synka din skirt då

--- app/src/main/java/com/johnsimon/payback/adapter/NavigationDrawerAdapter.java
+++ app/src/main/java/com/johnsimon/payback/adapter/NavigationDrawerAdapter.java
@@ -93,7 +93,7 @@ public class NavigationDrawerAdapter extends BaseAdapter {
 
 		Person owner = item.owner;
 
-		Resource.createProfileImage(owner, holder.avatar, holder.avatarLetter, context.getResources());
+		Resource.createProfileImage(owner, holder.avatar, holder.avatarLetter);
 
 		holder.title.setText(item.title);
 
