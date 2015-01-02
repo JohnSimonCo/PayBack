402c8362b31d725c51b12a5fcc9cd2d11aabb46c
John Rapp <johnfarnes@hotmail.com>
2015-01-03 00:38:12 +0100

    Fårenhär att funka och sen kommitar du!

--- app/src/main/java/com/johnsimon/payback/ui/NavigationDrawerFragment.java
+++ app/src/main/java/com/johnsimon/payback/ui/NavigationDrawerFragment.java
@@ -110,7 +110,7 @@ public class NavigationDrawerFragment extends DataFragment {
 		mDrawerListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 	        @Override
 	        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-		        selectItem(position + mDrawerListView.getHeaderViewsCount());
+		        selectItem(position - mDrawerListView.getHeaderViewsCount());
 	        }
         });
 
