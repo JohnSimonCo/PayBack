9bac2cef9c1b1166408c52b3830896db3c12f2b0
Simon Halvdansson <swesnowme@gmail.com>
2015-01-13 19:52:27 +0100

    Doesn't crash!

--- app/src/main/java/com/johnsimon/payback/util/SwishLauncher.java
+++ app/src/main/java/com/johnsimon/payback/util/SwishLauncher.java
@@ -4,17 +4,18 @@ import android.app.Activity;
 import android.content.ComponentName;
 import android.content.Intent;
 
+import com.johnsimon.payback.core.Debt;
+
 public class SwishLauncher {
 
-	public static void startSwish(Activity activity) {
+	public static void startSwish(Activity activity, Debt debt) {
 		Intent intent = new Intent(Intent.ACTION_MAIN);
 		intent.setComponent(ComponentName.unflattenFromString("se.bankgirot.swish/.ui.PaymentActivity"));
 		intent.addCategory(Intent.CATEGORY_LAUNCHER );
 
-		intent.putExtra("phone", "1234");
-		intent.putExtra("message", "bajs");
+		intent.putExtra("phone", "123");
 
-		startActivity(intent);
+		activity.startActivity(intent);
 	}
 
 }
