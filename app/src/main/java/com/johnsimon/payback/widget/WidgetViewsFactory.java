cfe2f03c49c8009d5ebfa1d2c835404c4909e320
SweSnow <swesnowme@gmail.com>
2015-02-22 18:18:23 +0100

    Widget layout progress

--- app/src/main/java/com/johnsimon/payback/widget/WidgetViewsFactory.java
+++ app/src/main/java/com/johnsimon/payback/widget/WidgetViewsFactory.java
@@ -9,6 +9,8 @@ import android.widget.RemoteViews;
 
 import com.johnsimon.payback.R;
 import com.johnsimon.payback.data.Debt;
+import com.johnsimon.payback.drawable.AvatarPlaceholderDrawable;
+import com.johnsimon.payback.util.ColorPalette;
 import com.johnsimon.payback.util.Resource;
 
 public class WidgetViewsFactory extends DataWidgetViewsFactory {
@@ -35,40 +37,34 @@ public class WidgetViewsFactory extends DataWidgetViewsFactory {
     public RemoteViews getViewAt(int position) {
         final RemoteViews row = new RemoteViews(context.getPackageName(), R.layout.widget_list_item);
 
-        /*
-
         Debt debt = data.debts.get(position);
 
         if (debt.getOwner().hasImage()) {
-    //        row.setImageViewUri(R.id.list_item_avatar, Uri.parse(debt.getOwner().link.photoURI));
+            row.setImageViewUri(R.id.list_widget_avatar, Uri.parse(debt.getOwner().link.photoURI));
         } else {
-//            row.setImageViewBitmap(R.id.list_item_avatar, Resource.drawableToBitmap(new AvatarPlaceholderDrawable(ColorPalette.getInstance(this), debt.getOwner().paletteIndex)));
-            row.setViewVisibility(R.id.list_item_avatar_letter, View.VISIBLE);
-            row.setTextViewText(R.id.list_item_avatar_letter, debt.getOwner().getAvatarLetter());
+        //    row.setImageViewBitmap(R.id.list_widget_avatar, Resource.drawableToBitmap(new AvatarPlaceholderDrawable(ColorPalette.getInstance(this), debt.getOwner().paletteIndex)));
+            row.setViewVisibility(R.id.list_widget_avatar_letter, View.VISIBLE);
+            row.setTextViewText(R.id.list_widget_avatar_letter, debt.getOwner().getAvatarLetter());
         }
 
-        row.setTextViewText(R.id.list_item_person, debt.getOwner().getName());
-        row.setTextViewText(R.id.list_item_note, debt.getNote() == null ? context.getResources().getString(R.string.cash) : debt.getNote());
-        row.setTextViewText(R.id.list_item_date, " - " + Resource.getRelativeTimeString(context, debt.timestamp));
-		row.setTextViewText(R.id.list_item_amount, data.preferences.getCurrency().render(debt));
+        row.setTextViewText(R.id.list_widget_person, debt.getOwner().getName());
+        row.setTextViewText(R.id.list_widget_note, debt.getNote() == null ? context.getResources().getString(R.string.cash) : debt.getNote());
+		row.setTextViewText(R.id.list_widget_amount, data.preferences.getCurrency().render(debt));
 
         if (debt.isPaidBack()) {
-            row.setTextColor(R.id.list_item_person, context.getResources().getColor(R.color.gray_text_very_light));
-            row.setTextColor(R.id.list_item_note, context.getResources().getColor(R.color.gray_oncolor_light));
-            row.setTextColor(R.id.list_item_amount, context.getResources().getColor(debt.getDisabledColor()));
+            row.setTextColor(R.id.list_widget_person, context.getResources().getColor(R.color.gray_text_very_light));
+            row.setTextColor(R.id.list_widget_note, context.getResources().getColor(R.color.gray_oncolor_light));
+            row.setTextColor(R.id.list_widget_amount, context.getResources().getColor(debt.getDisabledColor()));
         } else {
-            row.setTextColor(R.id.list_item_person, context.getResources().getColor(R.color.gray_text_normal));
-            row.setTextColor(R.id.list_item_note, context.getResources().getColor(R.color.gray_text_light));
-            row.setTextColor(R.id.list_item_amount, context.getResources().getColor(debt.getColor()));
+            row.setTextColor(R.id.list_widget_person, context.getResources().getColor(R.color.gray_text_normal));
+            row.setTextColor(R.id.list_widget_note, context.getResources().getColor(R.color.gray_text_light));
+            row.setTextColor(R.id.list_widget_amount, context.getResources().getColor(debt.getColor()));
         }
 
         Intent intent = new Intent();
-        Bundle extras = new Bundle();
 
-        //TODO nämen, här ståre bajs!
-        extras.putString(ListWidgetService.EXTRA_SEND, "bajs");
-        intent.putExtras(extras);
-        row.setOnClickFillInIntent(R.id.feed_list_item_master, intent);*/
+        intent.putExtra(ListWidgetService.EXTRA_OPEN, debt.getId());
+        row.setOnClickFillInIntent(R.id.feed_list_item_master, intent);
 
         return row;
     }
