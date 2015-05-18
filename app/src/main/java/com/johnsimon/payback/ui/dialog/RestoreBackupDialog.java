e107e7b10d21343a25b3b61f8cf8a091dbfbebe5
John Rapp <johnfarnes@hotmail.com>
2015-05-18 17:47:13 +0200

    Jag kan pusha

---
+++ app/src/main/java/com/johnsimon/payback/ui/dialog/RestoreBackupDialog.java
@@ -0,0 +1,34 @@
+package com.johnsimon.payback.ui.dialog;
+
+import android.content.Context;
+
+import com.afollestad.materialdialogs.MaterialDialog;
+import com.johnsimon.payback.async.Promise;
+import com.johnsimon.payback.data.AppData;
+import com.johnsimon.payback.storage.Storage;
+import com.johnsimon.payback.util.FileManager;
+
+public class RestoreBackupDialog {
+
+	public static Promise<Boolean> attemptRestore(Context context, final Storage storage) {
+		Promise<Boolean> p = new Promise<>();
+		final FileManager.ReadResult result = FileManager.read();
+		if(result.success) {
+			new MaterialDialog.Builder(context)
+					.title("You got a file bro")
+					.content("Wanna restore from it???")
+					.positiveText("Yes i am")
+					.negativeText("No, sure")
+					.callback(new MaterialDialog.ButtonCallback() {
+						@Override
+						public void onPositive(MaterialDialog dialog) {
+							storage.commit(AppData.fromJson(result.content));
+							storage.emit();
+							
+							p.fire(true);
+						}
+					});
+		}
+		return p;
+	}
+}
