25b6728af177d0d2bc44fe91810eced817a0becd
Simon Halvdansson <swesnowme@gmail.com>
2015-05-18 17:45:50 +0200

    Revert "Kolla här"
    This reverts commit 292f46e7c1e1163777b65cafe1ce270357b82a77.

--- app/src/main/java/com/johnsimon/payback/ui/dialog/WelcomeNfcDialogFragment.java
+++ app/src/main/java/com/johnsimon/payback/ui/dialog/WelcomeNfcDialogFragment.java
@@ -10,7 +10,6 @@ import android.view.View;
 import android.widget.Button;
 
 import com.johnsimon.payback.R;
-import com.johnsimon.payback.ui.FeedActivity;
 
 public class WelcomeNfcDialogFragment extends DialogFragment {
 
@@ -29,7 +28,6 @@ public class WelcomeNfcDialogFragment extends DialogFragment {
 			@Override
 			public void onClick(View v) {
 				alertDialog.dismiss();
-				//TODO KOLLA HÄR
 			}
 		});
 
