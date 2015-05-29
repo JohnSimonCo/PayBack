1efc168c43dbe2023ac9314090e8c28f9c3c7332
Simon Halvdansson <swesnowme@gmail.com>
2015-05-29 23:40:12 +0200

    LeakCanary less agressive

--- app/src/main/java/com/johnsimon/payback/core/BaseApplication.java
+++ app/src/main/java/com/johnsimon/payback/core/BaseApplication.java
@@ -2,11 +2,14 @@ package com.johnsimon.payback.core;
 
 import android.app.Application;
 
+import com.johnsimon.payback.BuildConfig;
 import com.squareup.leakcanary.LeakCanary;
 
 public class BaseApplication extends Application {
     @Override public void onCreate() {
         super.onCreate();
-        LeakCanary.install(this);
+        if (BuildConfig.DEBUG) {
+            LeakCanary.install(this);
+        }
     }
 }
