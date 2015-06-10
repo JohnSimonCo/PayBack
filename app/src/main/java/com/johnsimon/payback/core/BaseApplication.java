6f31c4a43130361739d6238fe53f433dbaee2dc5
SweSnow <swesnowme@gmail.com>
2015-06-10 12:54:14 +0200

    ACRA

--- app/src/main/java/com/johnsimon/payback/core/BaseApplication.java
+++ app/src/main/java/com/johnsimon/payback/core/BaseApplication.java
@@ -1,15 +1,36 @@
 package com.johnsimon.payback.core;
 
 import android.app.Application;
+import android.content.Context;
+
+import org.acra.*;
+import org.acra.annotation.*;
 
 import com.johnsimon.payback.BuildConfig;
 import com.squareup.leakcanary.LeakCanary;
 
+
+@ReportsCrashes(formUri = "https://collector.tracepot.com/195178b1")
+
+
 public class BaseApplication extends Application {
-    @Override public void onCreate() {
+
+    private static Context context;
+
+    @Override
+    public void onCreate() {
         super.onCreate();
+
+        ACRA.init(this);
+
+        BaseApplication.context = getApplicationContext();
+
         if (BuildConfig.DEBUG) {
             LeakCanary.install(this);
         }
+    }
+
+    public static Context getAppContext() {
+        return BaseApplication.context;
     }
 }
